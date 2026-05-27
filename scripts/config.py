58246894a84775e6d9e9cba8699aee2cdca93270
Kaito NAKABUCHI <nakabuchi.kaito.603@s.kyushu-u.ac.jp>
2026-05-27 15:39:33 +0900

    Backout unneeded changes
    Signed-off-by: Kaito NAKABUCHI <nakabuchi.kaito.603@s.kyushu-u.ac.jp>

--- scripts/config.py
+++ scripts/config.py
@@ -4,5 +4,4 @@ REPO_ROOT   = Path(__file__).resolve().parent.parent
 TR1UM_ROOT  = REPO_ROOT / "external" / "TR-1um"
 DRC_DIR     = TR1UM_ROOT / "libs.tech" / "klayout" / "drc"
 DRC_RUNSET  = DRC_DIR / "run.drc"
-#KLAYOUT_BIN = "/Applications/klayout.app/Contents/MacOS/klayout"
-KLAYOUT_BIN = r"C:\Users\buchi\AppData\Roaming\KLayout\klayout_app.exe"
+KLAYOUT_BIN = "/Applications/klayout.app/Contents/MacOS/klayout"
